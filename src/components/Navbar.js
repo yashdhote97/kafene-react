f7514af01f0c5ffa31c6653513cfd53431b44c82
yashdhote97 <68855202+yashdhote97@users.noreply.github.com>
2021-02-24 14:53:16 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link,Redirect } from "react-router-dom";
 import "../styles/Navbar.css";
+import {useSelector, useDispatch} from "react-redux";
+import {setLogin} from "../reducers";
 import logo from '../images/logo.58169365.png'
 
 const Navbar = () => {
+    let login=useSelector(state => state.loginStatus);
+    console.log(login)
+    const dispatch = useDispatch();
     return ( 
         <div id="topbar">
         <div id="topbarLeft">
@@ -11,12 +16,22 @@ const Navbar = () => {
                 <img src={logo} alt="Logo" />
                 <p className="brandName">Kafene</p>
             </div>
+            {login?(
                 <nav>
                     <Link to="/orders" className="topbarMenu">Orders</Link>
                     <Link to="/products" className="topbarMenu">Products</Link>
                     <Link to="/users" className="topbarMenu">Users</Link>
-                    <Link to="/" className="topbarMenu" id="logout">Logout</Link>
+                    <Link to="/" className="topbarMenu" id="logout" onClick={() => dispatch(setLogin(false))}>Logout</Link>
                 </nav>
+            ):(
+                <nav>
+                    <Link to="/" className="topbarMenu">Orders</Link>
+                    <Link to="/" className="topbarMenu">Products</Link>
+                    <Link to="/" className="topbarMenu">Users</Link>
+                    <Redirect to={'/'} />
+                </nav>
+                
+            )}
         </div>
     </div>
      );
